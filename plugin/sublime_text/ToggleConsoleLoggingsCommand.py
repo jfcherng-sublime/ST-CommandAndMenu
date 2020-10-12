9b787e398c30eb192a173ea83bf5fbfac6298604
Jack Cherng <jfcherng@gmail.com>
2020-10-12 17:51:57 +0800

    refactor: ToggleConsoleLoggingsCommand.py
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/sublime_text/ToggleConsoleLoggingsCommand.py
+++ plugin/sublime_text/ToggleConsoleLoggingsCommand.py
@@ -3,26 +3,41 @@ import sublime_plugin
 
 
 class ToggleConsoleLoggingsCommand(sublime_plugin.ApplicationCommand):
-    is_logging_enabled = False
-    loggings = set(filter(lambda method: method.startswith("log_"), dir(sublime)))
-
-    def is_checked(self) -> bool:
-        return self.is_logging_enabled
+    logging_methods = sorted(filter(lambda method: method.startswith("log_"), dir(sublime)))
 
     def description(self) -> str:
         return "Toggle all Sublime Text's logging functionalities."
 
-    def run(self, enable=...) -> None:
-        self.is_logging_enabled = not self.is_logging_enabled if enable is ... else bool(enable)
+    def run(self) -> None:
+        answer = sublime.yes_no_cancel_dialog("Enable / Disable all console logging methods?", "Enable", "Disable")
 
-        for logging in self.loggings:
+        if answer == sublime.DIALOG_CANCEL:
+            return
+
+        if answer == sublime.DIALOG_NO:
+            self._enable_all_logging_methods(False)
+            self._show_result_dialog(False)
+
+            return
+
+        if answer == sublime.DIALOG_YES:
+            self._enable_all_logging_methods(True)
+            self._show_result_dialog(True)
+
+            return
+
+    def _enable_all_logging_methods(self, enable: bool = True) -> None:
+        for method in self.logging_methods:
             try:
-                getattr(sublime, logging)(self.is_logging_enabled)
+                getattr(sublime, method)(enable)
             except Exception:
                 pass
 
+    def _show_result_dialog(self, answer: bool) -> None:
         sublime.message_dialog(
-            "Sublime Text loggings: "
-            + ("ON" if self.is_logging_enabled else "OFF")
-            + ("\n" + "-" * 30 + "\n" + "\n".join(self.loggings) if self.is_logging_enabled else "")
+            "Console loggings: {}{}{}".format(
+                ("enabled" if answer else "disabled") + "\n",
+                "-" * 30 + "\n",
+                "\n".join(self.logging_methods) + "\n",
+            )
         )
