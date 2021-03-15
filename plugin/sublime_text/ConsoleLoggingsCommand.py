325f2a8b7327e1a146e37c313e25e76defb857a9
Jack Cherng <jfcherng@gmail.com>
2021-03-15 19:43:53 +0800

    refactor: utilize ST 4099 get_log_*()
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/sublime_text/ConsoleLoggingsCommand.py
+++ plugin/sublime_text/ConsoleLoggingsCommand.py
@@ -1,105 +1,81 @@
+from abc import ABCMeta
+from typing import Callable, Optional
 import sublime
 import sublime_plugin
-
-from abc import ABCMeta
-from typing import List
 
 ST_METHODS = set(dir(sublime))
 ST_VERSION = int(sublime.version())
 
+ST_SUPPORT_GET_LOGGING_STATUS = ST_VERSION >= 4099
 
-class AbstractAskConsoleLoggingsCommand(sublime_plugin.ApplicationCommand, metaclass=ABCMeta):
-    row_separator = "-" * 30
+
+class AbstractToggleConsoleLoggingCommand(sublime_plugin.ApplicationCommand, metaclass=ABCMeta):
+    @property
+    def logging_method_name(self) -> str:
+        # strips the leading "toggle_" from the command name
+        return self.name()[7:]
+
+    @property
+    def logging_method(self) -> Callable[..., None]:
+        return getattr(sublime, self.logging_method_name)
+
+    @property
+    def get_logging_method(self) -> Callable[[], bool]:
+        return getattr(sublime, f"get_{self.logging_method_name}")
 
     def is_checked(self) -> bool:
-        # @todo there is no way to know whether a certain log_*() method is activated or not at this moment
-        return False
+        return self.get_logging_method()
 
     def is_enabled(self) -> bool:
-        # if there is a usable command
-        return any((method in ST_METHODS) for method in self.get_logging_method_names())
+        return ST_SUPPORT_GET_LOGGING_STATUS and (self.logging_method_name in ST_METHODS)
 
     def is_visible(self) -> bool:
         return self.is_enabled()
 
-    def get_logging_method_names(self) -> List[str]:
-        """ Gets names of logging methods. """
-
-        return [self.name()[4:]]  # strips the leading "ask_" from the command name
-
-    def run(self) -> None:
-        answer = sublime.yes_no_cancel_dialog(
-            "(De-)Activate console logging methods?\n{}{}".format(
-                self.row_separator + "\n",
-                "\n".join(self.get_logging_method_names()) + "\n",
-            ),
-            "Activate",
-            "Deactivate",
-        )
-
-        if answer == sublime.DIALOG_CANCEL:
-            return
-
-        self._enable_logging_methods(self.get_logging_method_names(), answer == sublime.DIALOG_YES)
-
-    def _enable_logging_methods(self, methods: List[str], enable: bool = True) -> None:
-        for method in methods:
-            try:
-                getattr(sublime, method)(enable)
-                print("{} console logging: {}".format("Activate" if enable else "Deactivate", method))
-            except Exception:
-                print(
-                    "Something wrong happened during {}: {}".format(
-                        "activating" if enable else "deactivating",
-                        method,
-                    )
-                )
+    def run(self, enable: Optional[bool] = None) -> None:
+        if enable is None:
+            self.logging_method()
+        else:
+            self.logging_method(enable)
 
 
-class AskLogAllCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating all logging commands. """
-
-    def get_logging_method_names(self) -> List[str]:
-        return sorted(filter(lambda method: method.startswith("log_"), dir(sublime)))
-
-
-class AskLogBuildSystemsCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_build_systems() """
+class ToggleLogBuildSystemsCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_build_systems() """
 
     ...
 
 
-class AskLogCommandsCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_commands() """
+class ToggleLogCommandsCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_commands() """
 
     ...
 
 
-class AskLogControlTreeCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_control_tree() """
+class ToggleLogControlTreeCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_control_tree() """
 
     ...
 
 
-class AskLogFpsCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_fps() """
+class ToggleLogFpsCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_fps() """
 
     ...
 
 
-class AskLogIndexingCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_indexing() """
+class ToggleLogIndexingCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_indexing() """
 
     ...
 
 
-class AskLogInputCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_input() """
+class ToggleLogInputCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_input() """
 
     ...
 
 
-class AskLogResultRegexCommand(AbstractAskConsoleLoggingsCommand):
-    """ Ask for activating sublime.log_result_regex() """
+class ToggleLogResultRegexCommand(AbstractToggleConsoleLoggingCommand):
+    """ Toggle for activating sublime.log_result_regex() """
 
     ...
