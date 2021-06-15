4182287ebc2c9dc85b4449b76f046aff1fdc94d4
Jack Cherng <jfcherng@gmail.com>
2021-06-15 20:54:51 +0800

    refactor: use pathlib in OpenSublimeTextDirCommand.py
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/OpenSublimeTextDirCommand.py
+++ plugin/OpenSublimeTextDirCommand.py
@@ -1,10 +1,10 @@
+from functools import lru_cache
+from pathlib import Path
+from typing import Dict
 import os
 import sublime
 import sublime_plugin
 import tempfile
-
-from functools import lru_cache
-from typing import Dict
 
 PACKAGE_NAME = __package__.split(".")[0]
 
@@ -12,34 +12,39 @@ PACKAGE_NAME = __package__.split(".")[0]
 @lru_cache
 def get_folder_map() -> Dict[str, str]:
     return {
-        k: os.path.realpath(v)
-        for k, v in {
+        name: str(path.resolve())
+        for name, path in {
             # from OS
-            "home": os.path.expanduser("~"),
-            "temp_dir": tempfile.gettempdir(),
+            "home": Path.home(),
+            "temp_dir": Path(tempfile.gettempdir()),
             # from ST itself
-            "bin": os.path.dirname(sublime.executable_path()),
-            "cache": sublime.cache_path(),
-            "data": os.path.join(sublime.packages_path(), ".."),
-            "index": os.path.join(sublime.cache_path(), "..", "Index"),
-            "installed_packages": sublime.installed_packages_path(),
-            "lib": os.path.join(sublime.packages_path(), "..", "Lib"),
-            "local": os.path.join(sublime.packages_path(), "..", "Local"),
-            "log": os.path.join(sublime.packages_path(), "..", "Log"),
-            "packages": sublime.packages_path(),
+            "bin": Path(sublime.executable_path()).parent,
+            "cache": Path(sublime.cache_path()),
+            "data": Path(sublime.packages_path(), ".."),
+            "index": Path(sublime.cache_path()) / ".." / "Index",
+            "installed_packages": Path(sublime.installed_packages_path()),
+            "lib": Path(sublime.packages_path()) / ".." / "Lib",
+            "local": Path(sublime.packages_path()) / ".." / "Local",
+            "log": Path(sublime.packages_path()) / ".." / "Log",
+            "packages": Path(sublime.packages_path()),
             # from LSP
-            "package_storage": os.path.join(sublime.cache_path(), "..", "Package Storage"),
+            "package_storage": Path(sublime.cache_path()) / ".." / "Package Storage",
         }.items()
     }
 
 
 class OpenSublimeTextDirCommand(sublime_plugin.ApplicationCommand):
-    def run(self, folder: str) -> None:  # type: ignore
-        path = sublime.expand_variables(folder, get_folder_map())
+    def run(self, folder: str) -> None:
+        window = sublime.active_window()
+        path = sublime.expand_variables(
+            folder,
+            {
+                **window.extract_variables(),
+                **get_folder_map(),
+            },
+        )
 
         if not os.path.isdir(path):
-            sublime.error_message(f"[{PACKAGE_NAME}] Directory not found: `{path}`")
+            return sublime.error_message(f"[{PACKAGE_NAME}] Directory not found: `{path}`")
 
-            return
-
-        sublime.active_window().run_command("open_dir", {"dir": path})
+        window.run_command("open_dir", {"dir": path})
