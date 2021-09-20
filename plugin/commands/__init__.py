79c11160d98ffbb718d33e993b864df564a8f290
Jack Cherng <jfcherng@gmail.com>
2021-09-20 20:45:16 +0800

    chore: clear menu items
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/commands/__init__.py
+++ plugin/commands/__init__.py
@@ -1,4 +1,4 @@
-from .clear_st_console import ClearStConsoleCommand  # noqa: F401
+from .clear_console import ClearConsoleCommand  # noqa: F401
 from .console_loggings import *  # noqa: F401, F403
 from .open_git_repo_on_web import OpenGitRepoOnWebCommand  # noqa: F401
 from .open_sublime_text_dir import OpenSublimeTextDirCommand  # noqa: F401
