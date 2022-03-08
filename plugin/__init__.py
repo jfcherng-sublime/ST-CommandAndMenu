2ce5e529bbb3224a18f8d4629c664e88eb4ffbbc
Jack Cherng <jfcherng@gmail.com>
2022-03-08 17:34:33 +0800

    refactor: simplify boot.py
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,11 +1,38 @@
-from .commands import *  # noqa: F401, F403
+# import all listeners and commands
+from .commands.clear_console import ClearConsoleCommand
+from .commands.console_loggings import ToggleLogBuildSystemsCommand
+from .commands.console_loggings import ToggleLogCommandsCommand
+from .commands.console_loggings import ToggleLogControlTreeCommand
+from .commands.console_loggings import ToggleLogFpsCommand
+from .commands.console_loggings import ToggleLogIndexingCommand
+from .commands.console_loggings import ToggleLogInputCommand
+from .commands.console_loggings import ToggleLogResultRegexCommand
+from .commands.open_git_repo_on_web import OpenGitRepoOnWebCommand
+from .commands.open_sublime_text_dir import OpenSublimeTextDirCommand
+from .commands.safe_mode import SafeModeCommand
+
+__all__ = (
+    # ST: core
+    "plugin_loaded",
+    "plugin_unloaded",
+    # ST: commands
+    "ClearConsoleCommand",
+    "OpenGitRepoOnWebCommand",
+    "OpenSublimeTextDirCommand",
+    "SafeModeCommand",
+    "ToggleLogBuildSystemsCommand",
+    "ToggleLogCommandsCommand",
+    "ToggleLogControlTreeCommand",
+    "ToggleLogFpsCommand",
+    "ToggleLogIndexingCommand",
+    "ToggleLogInputCommand",
+    "ToggleLogResultRegexCommand",
+)
 
 
-def set_up() -> None:
-    """plugin_loaded"""
-    ...
+def plugin_loaded() -> None:
+    pass
 
 
-def tear_down() -> None:
-    """plugin_unloaded"""
-    ...
+def plugin_unloaded() -> None:
+    pass
